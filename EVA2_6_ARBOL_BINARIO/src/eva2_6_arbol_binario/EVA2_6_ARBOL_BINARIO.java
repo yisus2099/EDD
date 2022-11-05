6d479b705e0b77614d36454a75e4114396c5ff34
yis19 <jesusalejandrot958@gmail.com>
2022-11-05 10:58:38 -0600

    Update EVA2_6_ARBOL_BINARIO.java

--- EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/EVA2_6_ARBOL_BINARIO.java
+++ EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/EVA2_6_ARBOL_BINARIO.java
@@ -12,6 +12,22 @@ public class EVA2_6_ARBOL_BINARIO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArbolBinario arbol = new ArbolBinario();
+        
+        arbol.Agregar(13);
+        arbol.Agregar(10);
+        arbol.Agregar(18);
+        arbol.Agregar(2);
+        arbol.Agregar(11);
+        arbol.Agregar(17);
+        arbol.Agregar(20);
+        arbol.Agregar(16);
+        arbol.Agregar(16);
+        
+        arbol.inOrder();
+        arbol.preOrder();
+        arbol.postOrder();
+        
     }
     
 }
