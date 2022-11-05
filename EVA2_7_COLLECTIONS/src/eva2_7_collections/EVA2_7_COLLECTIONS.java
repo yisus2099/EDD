566739767bd7618c40e971433080ce07aa3e3e31
yis19 <jesusalejandrot958@gmail.com>
2022-11-05 11:11:25 -0600

    Collections

--- EVA2_7_COLLECTIONS/src/eva2_7_collections/EVA2_7_COLLECTIONS.java
+++ EVA2_7_COLLECTIONS/src/eva2_7_collections/EVA2_7_COLLECTIONS.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package eva2_7_collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  *
@@ -15,6 +15,37 @@ public class EVA2_7_COLLECTIONS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LinkedList <String> listaEnlazada = new LinkedList();
+        System.out.println("LinkedList");
+        listaEnlazada.add("Hola");
+         listaEnlazada.add("+");
+        listaEnlazada.add("Mundo");
+        listaEnlazada.add("!!");
+        System.out.println(listaEnlazada);
+        System.out.println(listaEnlazada.get(0));
+        listaEnlazada.remove(0);
+        System.out.println(listaEnlazada);
+         listaEnlazada.addFirst("Hola");
+         System.out.println(listaEnlazada);
+         for (int i = 0; i < listaEnlazada.size(); i++) {
+             System.out.println(listaEnlazada.get(i) + " ");
+        }
+        
+         System.out.println("");
+         System.out.println("Arraylist");
+         //ArrayList
+         ArrayList arrayList = new ArrayList();
+         arrayList.add(100);
+        arrayList.add(200);
+        arrayList.add(300);
+        arrayList.add(400);
+        arrayList.add(500);
+        arrayList.add(600);
+        System.out.println(arrayList);
+        arrayList.add(2, 250);
+        System.out.println(arrayList);
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i) + " ");
+        } 
     }
-    
 }
