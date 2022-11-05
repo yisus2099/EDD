78065fb4c0eef9d7ce76b87b7e205e84dc02024d
yis19 <jesusalejandrot958@gmail.com>
2022-11-05 10:52:42 -0600

    Agregar recursivo

--- EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/ArbolBinario.java
+++ EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/ArbolBinario.java
@@ -1,4 +1,3 @@
-
 package eva2_6_arbol_binario;
 
 /**
@@ -6,11 +5,37 @@ package eva2_6_arbol_binario;
  * @author Jesús Alejandro Torres Ramírez
  */
 public class ArbolBinario {
+
     private Nodo root;
 
     public ArbolBinario() {
         this.root = null;
     }
-    
-    public 
+
+    public void Agregar(int valor) {
+        if (root == null) {
+            root = new Nodo(valor);
+        } else {
+            AgregarRecur(valor, root);
+        }
+    }
+
+    public void AgregarRecur(int valor, Nodo nodoActual) {
+        Nodo nuevo = new Nodo(valor);
+        if (valor < nodoActual.getValor()) {
+            if (nodoActual.getIzquierdo() == null) {
+                nodoActual.setIzquierdo(nuevo);
+            } else {
+                AgregarRecur(valor, nodoActual.getIzquierdo());
+            }
+        } else if (valor > nodoActual.getValor()) {
+            if (nodoActual.getDerecho() == null) {
+                nodoActual.setDerecho(nuevo);
+            } else {
+                AgregarRecur(valor, nodoActual.getDerecho());
+            }
+        } else {
+            System.out.println("ELEMENTO REPETIDO");
+        }
+    }
 }
