e2f51a5b515cc5d3f520a7543190013d54d86fdb
yis19 <jesusalejandrot958@gmail.com>
2022-11-05 10:47:29 -0600

    Clase Nodo

---
+++ EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/ArbolBinario.java
@@ -0,0 +1,16 @@
+
+package eva2_6_arbol_binario;
+
+/**
+ *
+ * @author Jesús Alejandro Torres Ramírez
+ */
+public class ArbolBinario {
+    private Nodo root;
+
+    public ArbolBinario() {
+        this.root = null;
+    }
+    
+    public 
+}
--- EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/Nodo.java
+++ EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/Nodo.java
@@ -8,5 +8,49 @@ package eva2_6_arbol_binario;
  * @author Jesús Alejandro Torres Ramírez
  */
 public class Nodo {
+
+    private int valor;
+    private Nodo derecho;
+    private Nodo izquierdo;
+
+    //Default, el nuevo nodo se va al final de la vista
+    //El final de la lista se indica con null
+    public Nodo() {
+        this.derecho = null;
+        this.izquierdo = null;
+    }
+
+    public Nodo(int valor) {
+        this.valor = valor;
+        this.derecho = null;
+        this.izquierdo = null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(Nodo derecho) {
+        this.derecho = derecho;
+    }
+
+    public Nodo getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(Nodo izquierdo) {
+        this.izquierdo = izquierdo;
+    }
     
+    
+    
+
 }
